d8c498b8f8c8072e84a49f0428d20bf02a324cbc
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-10-05 14:20:26 +0300

    implemented tad_list page and included link to sidebar

--- tasks_and_tags/urls.py
+++ tasks_and_tags/urls.py
@@ -4,7 +4,7 @@ from .views import TagListView
 
 urlpatterns = [
     # path("", homepage, name="homepage"),
-    path("tags/", TagListView, name="homepage"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "tasks_and_tags"
