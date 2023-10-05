223bd28c387b8c24acab55d97224b43d1b3b0441
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-10-05 15:41:59 +0300

    implemented task_list page and included link to sidebar

--- tasks_and_tags/urls.py
+++ tasks_and_tags/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import TagListView
+from .views import TagListView, TaskListView, TaskCreateView, TagCreateView
 
 urlpatterns = [
-    # path("", homepage, name="homepage"),
+    path("", TaskListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "tasks_and_tags"
--- tasks_and_tags/views.py
+++ tasks_and_tags/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
-from .models import Tag
+from .models import Tag, Task
 
 
 class TagListView(generic.ListView):
     model = Tag
 
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks_and_tags:task-list")
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks_and_tags:tag-list")
