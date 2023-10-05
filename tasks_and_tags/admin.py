3a4ec9105e633333e295594e0fde5e11111b675b
Igor Berezivskyi <igor200305.ber@gmail.com>
2023-10-05 12:38:05 +0300

    created superuser and registrated task and tag model in admin.py

--- tasks_and_tags/admin.py
+++ tasks_and_tags/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "task_created", "deadline", "is_task_done"]
+
+
+admin.site.register(Tag)
